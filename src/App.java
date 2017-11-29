80c905fe729d8ec4a5d81fa09a2808786f97de0e
codejunky92 <0851997@hr.nl>
2017-11-29 23:32:52 +0100

    almost done code

--- src/App.java
+++ src/App.java
@@ -3,8 +3,11 @@ public class App
     public static void main(String[] args)
     {
         UIntersection uInt = new UIntersection();
-        //uInt.getC();
-        uInt.createIntersectionPoint();
+        uInt.start();
+
+
+        //System.out.println(uInt.getC());
+        //uInt.createIntersectionPoints(8, 5);
         //uInt.intersectionPoint.getX();
         //uInt.intersectionPoint.getY();
     }
--- src/UIntersection.java
+++ src/UIntersection.java
@@ -1,55 +1,96 @@
 import java.awt.*;
-
 import static java.lang.Math.*;
 import static java.lang.StrictMath.toDegrees;
 
-public class UIntersection
+public class UIntersection extends Thread
 {
-    private Point firstCircleCenterPoint;
-    private Point secondCircleCenterPoint;
-    public Point intersectionPoint;
-
-    private int x1 = 0;
-    private int x2 = 6;
-    private int y1 = 0;
-    private int y2 = 6;
-
-
-    private double r1b = 4;
-    private double r2a = 5;
+    private int xUWB1 = 6, yUWB1 = 6;
+    private int xUWB2 = 13, yUWB2 = 14;
+    private int deltaX = (xUWB2-xUWB1);
+    private int deltaY = (yUWB2-yUWB1);
     private double c;
+
+    private double userLocationPointX1, userLocationPointY1;
+    private double userLocationPointX2, userLocationPointY2;
+
 
     public UIntersection()
     {
-        firstCircleCenterPoint = new Point(x1,y1);
-        secondCircleCenterPoint = new Point(x2,y2);
+
     }
 
-    public void createIntersectionPoint()
+
+    @Override
+    public void run()
     {
-        double cosAlpha = (pow(r2a,2)-pow(r1b,2)-pow(getC(),2))/(-2*r1b*getC());
-        double alpha = toDegrees(acos(cosAlpha));
-        //System.out.println(alpha);
-        double corigeren = toDegrees(atan(y2/x2));
-        double x = sin(alpha+corigeren)*r1b;
-        double y = sin(alpha)*r1b;
-        System.out.println(x);
-        intersectionPoint = new Point((int)x,(int)y);
+        while(true)
+        {
+            createIntersectionPoints(8, 5);
+            try
+            {
+                sleep(1000);
+            }
+            catch (InterruptedException e)
+            {
+                e.printStackTrace();
+            }
+        }
     }
+
+
+    public void createIntersectionPoints(double rUWB1, double rUWB2)
+    {
+        double alphaAngleDegree = toDegrees(acos((pow(rUWB1,2) + pow(getC(),2) - pow(rUWB2,2)) / (2*rUWB1*getC())));
+
+        //get (x3,y3) coordinates
+        double correctionAngle = toDegrees(atan(((double)deltaY)/((double)deltaX)));
+        double calculationAngle = alphaAngleDegree+correctionAngle;
+        userLocationPointX1 = rUWB1 * cos(toRadians(calculationAngle)) + xUWB1;
+        userLocationPointY1 = rUWB1 * sin(toRadians(calculationAngle)) + yUWB1;
+
+        //get (x4,x4) coordinates
+        double calculationAngle2 = 90 - calculationAngle + 2*alphaAngleDegree;
+        userLocationPointX2 = rUWB1 * sin(toRadians(calculationAngle2)) + xUWB1;
+        userLocationPointY2 = rUWB1 * cos(toRadians(calculationAngle2)) + yUWB1;
+
+        System.out.println("P3("+(float)userLocationPointX1+", "+(float)userLocationPointY1+")");
+        System.out.println("P4("+(float)userLocationPointX2+", "+(float)userLocationPointY2+")");
+    }
+
 
     public double getC()
     {
-        if((y2-y1)==0)
+        if((yUWB2-yUWB1)==0)
         {
-            c = x2-x1;
+            c = deltaX;
         }
         else
         {
-            c = sqrt(pow((x2-x1),2)+pow((y2-y1),2));
-            //System.out.println(c);
+            c = sqrt(pow((deltaX),2) + pow((deltaY),2));
         }
 
         return c;
+    }
+
+
+    public double getUserLocationPointX1()
+    {
+        return userLocationPointX1;
+    }
+
+    public double getUserLocationPointY1()
+    {
+        return userLocationPointY1;
+    }
+
+    public double getUserLocationPointX2()
+    {
+        return userLocationPointX2;
+    }
+
+    public double getUserLocationPointY2()
+    {
+        return userLocationPointY2;
     }
 
 }
